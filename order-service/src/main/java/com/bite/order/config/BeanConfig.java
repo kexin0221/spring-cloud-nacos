95c6de02786d33c413398f62ac9724bc4ff431bb
mou-kexin <2584857586@qq.com>
2026-08-31 12:28:02 +0800

    nacos学习代码

--- order-service/src/main/java/com/bite/order/config/BeanConfig.java
+++ order-service/src/main/java/com/bite/order/config/BeanConfig.java
@@ -1,7 +1,7 @@
 package com.bite.order.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
