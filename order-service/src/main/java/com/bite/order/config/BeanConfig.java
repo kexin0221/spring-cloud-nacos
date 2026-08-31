625413e91e142fae64bffdacd09d873be5a9d9b7
mou-kexin <2584857586@qq.com>
2026-08-31 16:00:50 +0800

    nacos学习代码

--- order-service/src/main/java/com/bite/order/config/BeanConfig.java
+++ order-service/src/main/java/com/bite/order/config/BeanConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@LoadBalancerClient(name = "product-service", configuration = CustomLoadBalancerConfiguration.class)
 @Configuration
 public class BeanConfig {
     @Bean
--- order-service/src/main/java/com/bite/order/service/OrderService.java
+++ order-service/src/main/java/com/bite/order/service/OrderService.java
@@ -5,14 +5,8 @@ import com.bite.order.model.OrderInfo;
 import com.bite.order.model.ProductInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Service
@@ -23,21 +17,10 @@ public class OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    // 计数器
-    private static AtomicInteger count = new AtomicInteger(1);
-
     public OrderInfo selectOrderById(Integer orderId) {
         OrderInfo orderInfo = orderMapper.selectOrderById(orderId);
-//        // 根据应用名称获取服务列表
-//        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
-//        // 服务可能有多个，轮询获得实例
-//        int index = count.getAndIncrement() % instances.size();
-//        String uri = instances.get(index).getUri().toString();
         String url = "http://product-service/product/" + orderInfo.getProductId();
-        log.info("远程调用url:{}", url);
+        log.info("url:{}", url);
         ProductInfo productInfo = restTemplate.getForObject(url, ProductInfo.class);
         orderInfo.setProductInfo(productInfo);
         return orderInfo;
